d8422529b5f128f6cd945a1aa6ab393af30f1fb6
Robert Santos <robertsantos.dfw@gmail.com>
2024-06-26 20:40:18 -0500

    Made changes to Masthead

--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -6,10 +6,9 @@ export default function Masthead() {
     <header className="masthead">
       <div className="container d-flex h-100 align-items-center">
         <div className="mx-auto text-center">
-          <h1 className="mx-auto my-0 text-uppercase">Full Stack Developer</h1>
+          <h1 className="mx-auto my-0 text-uppercase">Software Enthusiast</h1>
           <h2 className="text-white-50 mx-auto mt-2 mb-5">
-            A talented Developer with excitment working on backend and
-            middle-ware code.{" "}
+            Skilled Technical Product Manager who builds impactful and insightful Software.{" "}
           </h2>
         </div>
       </div>
